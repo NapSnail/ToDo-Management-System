490102bab2a64e16617f5721acac75c82d8aae9d
YukaUeno <156647522+NapSnail@users.noreply.github.com>
2024-03-03 19:29:18 +0900

    ユーザーログイン設定確認完了

---
+++ src/main/java/com/dmm/task/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.dmm.task.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	// ログイン確認用（後で消しておく）
+	@GetMapping("/main")
+	public String mainPage() {
+		return "main";
+	}
+
+}
