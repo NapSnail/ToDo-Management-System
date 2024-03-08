9ad7b2d43ee77b0a260727e46ba3ca3155f80e72
YukaUeno <156647522+NapSnail@users.noreply.github.com>
2024-03-09 00:51:22 +0900

    課題再提出

--- src/main/java/com/dmm/task/controller/MainController.java
+++ src/main/java/com/dmm/task/controller/MainController.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,30 +54,18 @@ public class MainController {
 
 		Timer timer = new Timer(toMonth);
 		List<List<LocalDate>> matrix = new ArrayList<>();
-		List<LocalDate> week1 = new ArrayList<>();
-		List<LocalDate> week2 = new ArrayList<>();
-		List<LocalDate> week3 = new ArrayList<>();
-		List<LocalDate> week4 = new ArrayList<>();
-		List<LocalDate> week5 = new ArrayList<>();
-		List<LocalDate> week6 = new ArrayList<>();
-
-		week1 = timer.week1();
-		week2 = timer.week2();
-		week3 = timer.week3();
-		week4 = timer.week4();
-		week5 = timer.week5();
-		week6 = timer.week6();
-
-		matrix.addAll(Arrays.asList(week1, week2, week3, week4, week5, week6));
+		
+		timer.listSet(matrix);
+		
 		model.addAttribute("matrix", matrix);
 
 		MultiValueMap<LocalDate, Tasks> tasks = new LinkedMultiValueMap<LocalDate, Tasks>();
-		List<Tasks> task = trepo.findByDateBetween(week1.get(0).atStartOfDay(), week6.get(6).atStartOfDay(),
+		List<Tasks> task = trepo.findByDateBetween(matrix.get(0).get(0).atStartOfDay(), matrix.get(matrix.size() - 1).get(6).atStartOfDay(),
 				user.getUsername());
 
 		if (user.getUsername().equals("admin")) {
 
-			task = trepo.findByDateBetweenAd(week1.get(0).atStartOfDay(), week6.get(6).atStartOfDay());
+			task = trepo.findByDateBetweenAd(matrix.get(0).get(0).atStartOfDay(), matrix.get(matrix.size() - 1).get(6).atStartOfDay());
 		}
 
 		timer.setUpTasks(tasks, task);
--- src/main/java/com/dmm/task/data/Timer.java
+++ src/main/java/com/dmm/task/data/Timer.java
@@ -13,113 +13,121 @@ import lombok.Data;
 
 @Data
 public class Timer {
-	
+
 	private LocalDate toDay = LocalDate.now();
 	private LocalDate firstDay = toDay.withDayOfMonth(1);
 	private LocalDate lastDay = firstDay.plusDays(toDay.lengthOfMonth() - 1);
-	
+
 	public Timer(LocalDate date) {
 		this.toDay = date;
 		this.firstDay = toDay.withDayOfMonth(1);
 		this.lastDay = firstDay.plusDays(toDay.lengthOfMonth() - 1);
 	}
-	
-	//その月のカレンダーの最初になる日
+
+	// その月のカレンダーの最初になる日
 	public LocalDate callenderTopDay() {
+		LocalDate gone;
 		DayOfWeek whatTopDay = this.firstDay.getDayOfWeek();
-		LocalDate gone = this.firstDay.minusDays(whatTopDay.getValue());
-		return gone;
+		if (whatTopDay == DayOfWeek.SUNDAY) {
+			gone = this.firstDay;
+			return gone;
+		} else {
+			gone = this.firstDay.minusDays(whatTopDay.getValue());
+			return gone;
+		}
 	}
-	
-	//その月のカレンダーの最後になる日
+
+	// その月のカレンダーの最後になる日
 	public LocalDate callenderEndDay() {
-		DayOfWeek whatToDay = this.lastDay.getDayOfWeek();
-		LocalDate will = this.lastDay.plusDays(whatToDay.getValue() - 1);
-		return will;
+		LocalDate will;
+		DayOfWeek whatEndDay = this.lastDay.getDayOfWeek();
+		if(whatEndDay == DayOfWeek.SATURDAY) {
+			will = this.lastDay;
+			return will;
+		}else if(whatEndDay == DayOfWeek.SUNDAY) {
+			will = this.lastDay.plusDays(6);
+			return will;
+		}else {
+			will = this.lastDay.plusDays(6 - whatEndDay.getValue());
+			return will;
+		}
 	}
-	
+
+	// タスクを日付にセットする
 	public void setUpTasks(MultiValueMap<LocalDate, Tasks> tasks, List<Tasks> tasklist) {
 		LocalDate gone = callenderTopDay();
 		LocalDate will = callenderEndDay().plusDays(1);
 		LocalDate insert = gone;
-		
-		while(true) {
-			
-			if(insert.equals(will)) {
+
+		while (true) {
+
+			if (insert.equals(will)) {
 				break;
 			}
-			
+
 			tasks.add(insert, null);
-			
-			for(Tasks task :tasklist) {
-				if(task.getDate().equals(insert.atStartOfDay())) {
+
+			for (Tasks task : tasklist) {
+				if (task.getDate().equals(insert.atStartOfDay())) {
 					tasks.add(insert, task);
 				}
 			}
-			
+
 			insert = insert.plusDays(1);
 		}
 	}
-	
-	
-	//リスト詰め詰めループども。どうにかならんかったんか…( ﾟДﾟ)
-	public List<LocalDate> week1() {
-		List<LocalDate> weekDay = new ArrayList<>();
-		for (int i = 0; i < 7; i++) {
-			LocalDate insertDay = callenderTopDay().plusDays(i);
-			weekDay.add(insertDay);
-		}
-		return weekDay;
-	}
-	
-	public List<LocalDate> week2() {
-		List<LocalDate> weekDay = new ArrayList<>();
-		for (int i = 0; i < 7; i++) {
-			int roop2 = (7 * 1) + i;
-			LocalDate insertDay = callenderTopDay().plusDays(roop2);
-			weekDay.add(insertDay);
-		}
-		return weekDay;
-	}
-	
-	public List<LocalDate> week3() {
-		List<LocalDate> weekDay = new ArrayList<>();
-		for (int i = 0; i < 7; i++) {
-			int roop3 = (7 * 2) + i;
-			LocalDate insertDay = callenderTopDay().plusDays(roop3);
-			weekDay.add(insertDay);
-		}
-		return weekDay;
-	}
-	
-	public List<LocalDate> week4() {
-		List<LocalDate> weekDay = new ArrayList<>();
-		for (int i = 0; i < 7; i++) {
-			int roop4 = (7 * 3) + i;
-			LocalDate insertDay = callenderTopDay().plusDays(roop4);
-			weekDay.add(insertDay);
-		}
-		return weekDay;
-	}
-	
-	public List<LocalDate> week5() {
-		List<LocalDate> weekDay = new ArrayList<>();
-		for (int i = 0; i < 7; i++) {
-			int roop5 = (7 * 4) + i;
-			LocalDate insertDay = callenderTopDay().plusDays(roop5);
-			weekDay.add(insertDay);
-		}
-		return weekDay;
-	}
-	
-	public List<LocalDate> week6() {
-		List<LocalDate> weekDay = new ArrayList<>();
-		for (int i = 0; i < 7; i++) {
-			int roop6 = (7 * 5) + i;
-			LocalDate insertDay = callenderTopDay().plusDays(roop6);
-			weekDay.add(insertDay);
-		}
-		return weekDay;
-	}
 
+	// matrix用リストを作成する
+	public void listSet(List<List<LocalDate>> matrix){
+		LocalDate gone = callenderTopDay();
+		LocalDate will = callenderEndDay();
+		LocalDate insert = gone;
+
+		List<LocalDate> week1 = new ArrayList<>();
+		for (int i = 0; i < 7; i++) {
+			insert = callenderTopDay().plusDays(i);
+			week1.add(insert);
+		}
+		matrix.add(week1);
+
+		List<LocalDate> week2 = new ArrayList<>();
+		for (int i = 0; i < 7; i++) {
+			insert = insert.plusDays(1);
+			week2.add(insert);
+		}
+		matrix.add(week2);
+
+		List<LocalDate> week3 = new ArrayList<>();
+		for (int i = 0; i < 7; i++) {
+			insert = insert.plusDays(1);
+			week3.add(insert);
+		}
+		matrix.add(week3);
+
+		List<LocalDate> week4 = new ArrayList<>();
+		for (int i = 0; i < 7; i++) {
+			insert = insert.plusDays(1);
+			week4.add(insert);
+		}
+		matrix.add(week4);
+
+		if (insert.isBefore(will)) {
+			List<LocalDate> week5 = new ArrayList<>();
+			for (int i = 0; i < 7; i++) {
+				insert = insert.plusDays(1);
+				week5.add(insert);
+			}
+			matrix.add(week5);
+
+			if (insert.isBefore(will)) {
+				List<LocalDate> week6 = new ArrayList<>();
+				for (int i = 0; i < 7; i++) {
+					insert = insert.plusDays(1);
+					week6.add(insert);
+				}
+				matrix.add(week6);
+			}
+		}
+		
+	};
 }
